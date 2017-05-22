d250582b4cd90c9b03b095c17675c6f58d405cf2
Chouffe <chouffe.caillau@gmail.com>
2017-05-22 02:13:25 -0700

    Initial commit

---
+++ src/Descent.hs
@@ -0,0 +1,30 @@
+module Descent where
+
+import           Control.Monad
+import           Data.Bifunctor (bimap)
+import           Data.Foldable
+import           System.IO
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
+
+    -- The while loop represents the game.
+    -- Each iteration represents a turn of the game
+    -- where you are given inputs (the heights of the mountains)
+    -- and where you have to print an output (the index of the mountain to fire on)
+    -- The inputs you are given are automatically updated according to your last actions.
+
+    loop
+
+mountainHeights :: Int -> IO [Int]
+mountainHeights n = replicateM n $ read <$> getLine
+
+maximumIndex :: (Ord a) => [a] -> Int
+maximumIndex xs = snd $ maximumBy (\a b -> compare (fst a) (fst b)) $ zip xs [0..]
+
+loop :: IO ()
+loop = do
+    xs <- mountainHeights 8
+    putStrLn $ show $ maximumIndex xs
+    loop
