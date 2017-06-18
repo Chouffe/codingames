aa9ef807bad2896709ea8b97620ee54494742864
Chouffe <chouffe.caillau@gmail.com>
2017-06-18 00:31:58 -0700

    [CGXFormatter] finish pretty printer
    WIP: add spaces on key value pairs

--- src/CGXFormatter.hs
+++ src/CGXFormatter.hs
@@ -3,14 +3,11 @@
 module CGXFormatter where
 
 import           Control.Applicative
-import           Control.Monad
 import qualified Data.List                     as L
-import qualified Data.Map                      as M
-import           Data.Maybe                    (catMaybes, isJust)
-import qualified Debug.Trace                   as T
-import           Safe                          (atMay, headMay)
+-- import qualified Debug.Trace                   as T
 import           System.IO
 import qualified Text.ParserCombinators.Parsec as P
+import qualified Text.PrettyPrint              as PP
 
 -- Data Modeling
 
@@ -42,10 +39,12 @@ parseFalse = P.string "false" *> return (B False)
 parseBool :: P.Parser Primitive
 parseBool = parseTrue <|> parseFalse
 
-parseString :: P.Parser Primitive
-parseString = P.between (P.char '\'') (P.char '\'') $
-              S <$> P.many P.letter
+parseStr :: P.Parser String
+parseStr = P.between (P.char '\'') (P.char '\'') $
+           P.many (P.noneOf "\'")
 
+parseString :: P.Parser Primitive
+parseString = S <$> parseStr
 
 parsePrimitive :: P.Parser CGXValue
 parsePrimitive = CGXPrimitive <$>
@@ -57,28 +56,79 @@ parsePrimitive = CGXPrimitive <$>
 parseBlock :: P.Parser CGXValue
 parseBlock = do
     _ <- P.char '('
+    P.skipMany P.space
     values <- P.sepBy parseCGXValue (P.char ';')
+    P.skipMany P.space
     _ <- P.char ')'
     return (CGXBlock values)
 
 parseKeyValue :: P.Parser CGXValue
 parseKeyValue = do
-    key <- P.many P.letter
+    key <- parseStr
     _ <- P.char '='
     value <- parseCGXValue
     return $ CGXKeyValue key value
 
 parseCGXValue :: P.Parser CGXValue
-parseCGXValue = parsePrimitive <|>
-                parseBlock <|>
-                parseKeyValue
+parseCGXValue =
+  P.skipMany P.newline *>
+  P.skipMany P.tab *>
+  P.skipMany P.space *>
+    (P.try parseKeyValue <|>
+      parseBlock <|>
+      parsePrimitive)
 
 readCGX :: String -> Maybe CGXValue
 readCGX str = case P.parse parseCGXValue "cgx" str of
                 Left _  -> Nothing
                 Right v -> Just v
 
+readCGX' :: String -> Either P.ParseError CGXValue
+readCGX' = P.parse parseCGXValue "cgx"
+
 -- Pretty Printer
 
+indent :: Int -> PP.Doc -> PP.Doc
+indent k doc = PP.hsep $ (take k $ L.repeat (PP.text "")) ++ [doc]
+
+pprintHelper :: Int -> CGXValue -> PP.Doc
+pprintHelper k (CGXPrimitive Null)      = indent k $ PP.text "null"
+pprintHelper k (CGXPrimitive (B True))  = indent k $ PP.text "true"
+pprintHelper k (CGXPrimitive (B False)) = indent k $ PP.text "false"
+pprintHelper k (CGXPrimitive (N l))     = indent k $ PP.int l
+pprintHelper k (CGXPrimitive (S str))   = indent k $ PP.quotes $ PP.text str
+pprintHelper k (CGXBlock [])            = indent k (PP.lparen PP.<> PP.text "\n") PP.<> indent k PP.rparen
+pprintHelper k (CGXBlock xs)            =
+    indent k PP.lparen PP.<>
+    PP.hcat (PP.punctuate PP.semi $ elements xs) PP.<>
+    PP.text "\n" PP.<> (indent k PP.rparen)
+  where
+      elements :: [CGXValue] -> [PP.Doc]
+      elements = fmap (\e -> (PP.text "\n" PP.<> (pprintHelper (k + 4) e)))
+pprintHelper k (CGXKeyValue key val@(CGXBlock _)) =
+    indent k (PP.quotes (PP.text key) PP.<> PP.equals) PP.<>
+    PP.text "\n" PP.<> pprintHelper k val
+pprintHelper k (CGXKeyValue key val)              =
+    indent k (PP.quotes (PP.text key) PP.<> PP.equals) PP.<>
+    pprintHelper 0 val
+
+pprint :: CGXValue -> PP.Doc
+pprint = pprintHelper 0
 
 -- Game Logic
+
+debugGameInput :: CGXValue -> IO ()
+debugGameInput cgx = do
+  hPutStrLn stderr $ "Parsed CGX Value: " ++ show cgx
+  hPutStrLn stderr $ "Pretty Printed CGX Value: " ++ show (pprint cgx)
+
+gameInput :: IO (Maybe CGXValue)
+gameInput = getLine >> fmap readCGX getContents
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering
+    mcgx <- gameInput
+    case mcgx of
+      Nothing  -> fail "could not parse cgx file"
+      Just cgx -> debugGameInput cgx >> print (pprint cgx)
--- test/CGXFormatterSpec.hs
+++ test/CGXFormatterSpec.hs
@@ -1,12 +1,62 @@
 module CGXFormatterSpec (spec) where
 
 import           CGXFormatter
-import qualified Data.List       as L
-import qualified Data.Set        as S
+import           Control.Monad
 import           Test.Hspec
-import           Test.QuickCheck
+
+puzzles :: [(Int, CGXValue)]
+puzzles = [ (1, CGXPrimitive (B True))
+          , (2, CGXPrimitive (S " Content with spaces and\ttabs"))
+          , (3, CGXBlock [CGXPrimitive (N 0)])
+          , (4, CGXBlock [CGXPrimitive (N 0), CGXPrimitive (N 1), CGXPrimitive (N 2)])
+          , (5, CGXBlock [CGXBlock [CGXPrimitive (B True)]])
+          , (6, CGXBlock [])
+          , (7, CGXBlock [ CGXBlock [CGXPrimitive (B True)]
+                         , CGXBlock [CGXPrimitive (B False)]
+                         , CGXBlock [CGXPrimitive (N 0)]
+                         ])
+          , (8, CGXBlock [CGXKeyValue "key" (CGXPrimitive (S "value"))])
+          , (9, CGXBlock [ CGXKeyValue "k1" (CGXPrimitive (S "v1"))
+                         , CGXKeyValue "k2" (CGXPrimitive (S "v2"))
+                         , CGXPrimitive (N 123)
+                         , CGXKeyValue "k3" (CGXPrimitive (S "v3"))
+                         ])
+          , (10, (CGXKeyValue "users"
+          (CGXBlock [CGXBlock [CGXKeyValue "id" (CGXPrimitive (N 10)),CGXKeyValue "name" (CGXPrimitive (S "Serge")),CGXKeyValue "roles" (CGXBlock [CGXPrimitive (S "visitor"),CGXPrimitive (S "moderator")])],CGXBlock [CGXKeyValue "id" (CGXPrimitive (N 11)),CGXKeyValue "name" (CGXPrimitive (S "Biales"))],CGXPrimitive (B True)])))
+          , (11, (CGXBlock [CGXKeyValue "menu" (CGXBlock [CGXKeyValue "id" (CGXPrimitive (S "file")),CGXKeyValue "value" (CGXPrimitive (S "File")),CGXKeyValue "popup" (CGXBlock [CGXKeyValue "menuitem" (CGXBlock [CGXBlock [CGXKeyValue "value" (CGXPrimitive (S "New")),CGXKeyValue "onclick" (CGXPrimitive (S "CreateNewDoc()"))],CGXBlock [CGXKeyValue "value" (CGXPrimitive (S "Open")),CGXKeyValue "onclick" (CGXPrimitive (S "OpenDoc()"))],CGXBlock [CGXKeyValue "value" (CGXPrimitive (S "Close")),CGXKeyValue "onclick" (CGXPrimitive (S "CloseDoc()"))]])]),CGXBlock []])]))
+          , (12, (CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXBlock [CGXPrimitive (N 0)]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]))
+          ]
+
+specPath :: Int -> FilePath
+specPath i = "test/input/cgxformatter/spec_" ++ show i ++ ".txt"
+
+specPathOutput :: Int -> FilePath
+specPathOutput i = "test/output/cgxformatter/spec_" ++ show i ++ ".txt"
+
+
+withSpecInput :: Int -> String -> (String -> Expectation) -> Spec
+withSpecInput i title handler =
+  describe ("spec " ++ show i) $ do
+    it title $ do
+      content <- readFile (specPath i)
+      handler content
+
+specInputOutput :: Int -> Spec
+specInputOutput i =
+  describe ("spec " ++ show i) $ do
+    it "pretty prints properly" $ do
+      input <- readFile (specPath i)
+      output <- readFile (specPathOutput i)
+      case readCGX' input of
+        Left e    -> fail $ show e
+        Right cgx -> show (pprint cgx) ++ "\n" `shouldBe` output
+
 
 spec :: Spec
 spec =
   describe "CGXFormatter" $ do
-    it "fails" $ 1 `shouldBe` 0
+    forM_ puzzles (\(i, expected) -> do
+      withSpecInput i "parses properly"
+        (\input -> readCGX' input `shouldBe` Right expected))
+
+    forM_ [1..12] specInputOutput
